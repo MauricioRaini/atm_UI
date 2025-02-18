aa31e04e583e01061a221c2427b54e69536c7f47
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-17 22:04:47 -0600

    [ATM-2] fix(Jest): absolute imports

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ export default {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   moduleNameMapper: {
-    "\\.(css|scss|sass)$": "<rootDir>/config/CSSStub.js"
+    "\\.(css|scss|sass)$": "<rootDir>/config/CSSStub.js",
+    "^@/(.*)$": "<rootDir>/src/$1"
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  modulePaths: ["<rootDir>/src"]
 };
