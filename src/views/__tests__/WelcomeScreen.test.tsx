0bdaaeadcd8cf184420a18540b5e518ead4228ef
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-18 23:19:53 -0600

    [ATM-3] new(WelcomeScreen.test): initial failing test

---
+++ src/views/__tests__/WelcomeScreen.test.tsx
@@ -0,0 +1,40 @@
+import { renderHook, act, render } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { useBlueScreenStore } from "@/store/BlueScreenStore";
+import { WelcomeScreen } from "../WelcomeScreen/WelcomeScreen";
+import { PINEntryScreen } from "../PINEntryScreen/PINEntryScreen";
+import { NoCardMenu } from "../NoCardMenu/NoCardMenu";
+
+describe("🏦 Welcome Screen", () => {
+  beforeEach(() => {
+    act(() => {
+      useBlueScreenStore.setState({
+        screenContent: <WelcomeScreen />,
+        buttonBindings: {},
+        isAuthenticated: false,
+      });
+    });
+  });
+
+  it.skip("✅ Should navigate to PIN Entry when 'Enter PIN' is clicked", () => {
+    render(<WelcomeScreen />);
+    const { result } = renderHook(() => useBlueScreenStore());
+
+    act(() => {
+      result.current.buttonBindings[6]?.action?.();
+    });
+
+    expect(result.current.screenContent).toEqual(<PINEntryScreen />);
+  });
+
+  it.skip("✅ Should navigate to No Card Services when 'No Card Services' is clicked", () => {
+    render(<WelcomeScreen />);
+    const { result } = renderHook(() => useBlueScreenStore());
+
+    act(() => {
+      result.current.buttonBindings[7]?.action?.();
+    });
+
+    expect(result.current.screenContent).toEqual(<NoCardMenu />);
+  });
+});
