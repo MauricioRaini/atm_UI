8ed5f5fac5fa272b074d59720ec3221ab9e8de03
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-18 23:25:00 -0600

    [ATM-3] new(WelcomeScreen): first view

---
+++ src/views/WelcomeScreen/WelcomeScreen.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+import { useBlueScreenStore } from "@/store/BlueScreenStore";
+import { DynamicLabel } from "@/components/DynamicLabel";
+import { FONT_SIZES } from "@/types";
+
+export const WelcomeScreen = () => {
+  const { navigateTo, clearButtonBindings, setButtonBinding, setScreenContent } =
+    useBlueScreenStore();
+
+  useEffect(() => {
+    clearButtonBindings();
+
+    setScreenContent(
+      <DynamicLabel animated size={FONT_SIZES.lg}>
+        Welcome to the ATM
+      </DynamicLabel>,
+    );
+
+    setButtonBinding(3, {
+      label: "Enter PIN",
+      action: () => navigateTo("PINEntryScreen", true),
+    });
+
+    setButtonBinding(7, {
+      label: "No Card Services",
+      action: () => navigateTo("NoCardMenu", false),
+    });
+  }, [navigateTo, clearButtonBindings, setButtonBinding, setScreenContent]);
+
+  return null;
+};
