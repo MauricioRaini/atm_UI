13016732222ca71c3a978e01317d08147f06099f
Mauricio Raini <mauricioraini777@gmail.com>
2025-02-19 00:35:55 -0600

    [ATM-3] new(NumericKeyboard): fully functional component

--- src/components/NumericKeyboard/NumericKeyboard.tsx
+++ src/components/NumericKeyboard/NumericKeyboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, memo } from "react";
 import { ATMButton } from "@/components/ATMButton";
 import "./NumericKeyboard.css";
 
@@ -8,40 +8,42 @@ export type NumericKeyboardProps = {
   onClearPress: () => void;
 };
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+export const NumericKeyboard = memo(
+  ({ onNumberPress, onEnterPress, onClearPress }: NumericKeyboardProps) => {
+    const numberKeys = useMemo(() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 0], []);
 
-export const NumericKeyboard = ({
-  onNumberPress,
-  onEnterPress,
-  onClearPress,
-}: NumericKeyboardProps) => {
-  useEffect(() => {
-    const handleKeyPress = (event: KeyboardEvent) => {
-      if (event.key >= "0" && event.key <= "9") {
-        onNumberPress(Number(event.key));
-      } else if (event.key === "Enter") {
-        onEnterPress();
-      } else if (event.key === "Backspace") {
-        onClearPress();
-      }
-    };
+    useEffect(() => {
+      const handleKeyPress = (event: KeyboardEvent) => {
+        if (event.key >= "0" && event.key <= "9") {
+          onNumberPress(Number(event.key));
+        } else if (event.key === "Enter") {
+          onEnterPress();
+        } else if (event.key === "Backspace") {
+          onClearPress();
+        }
+      };
 
-    window.addEventListener("keydown", handleKeyPress);
-    return () => window.removeEventListener("keydown", handleKeyPress);
-  }, [onNumberPress, onEnterPress, onClearPress]);
+      window.addEventListener("keydown", handleKeyPress);
+      return () => window.removeEventListener("keydown", handleKeyPress);
+    }, [onNumberPress, onEnterPress, onClearPress]);
 
-  return (
-    <div className="numeric-keyboard">
-      <div className="number-grid">
-        {numbers.map((num) => (
-          <ATMButton key={num} label={String(num)} onClick={() => onNumberPress(num)} hidePath />
-        ))}
+    return (
+      <div className="keyboard-container">
+        <div className="numeric-keyboard">
+          <div className="number-grid">
+            {numberKeys.map((num) => (
+              <ATMButton
+                key={num}
+                label={String(num)}
+                onClick={() => onNumberPress(num)}
+                hidePath
+              />
+            ))}
+            <ATMButton label="Clear" onClick={onClearPress} hidePath />
+            <ATMButton label="Enter" onClick={onEnterPress} hidePath />
+          </div>
+        </div>
       </div>
-
-      <div className="bottom-buttons">
-        <ATMButton label="Enter" onClick={onEnterPress} hidePath />
-        <ATMButton label="Clear" onClick={onClearPress} hidePath />
-      </div>
-    </div>
-  );
-};
+    );
+  },
+);
